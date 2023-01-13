8f6e9433608d96eec99f5fce49086f806f257522
bigen1925 <bigen1925@gmail.com>
2023-01-13 17:43:52 +0900

    add
    とりあえず明滅するようにした

--- christmas-lights/src/App.tsx
+++ christmas-lights/src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [status, setStatus] = useState("off");
+  const [animation, setAnimation] = useState("");
+
+  function onClickHandler(value: string): void {
+    if (value === "on") {
+      setAnimation("flash 1.5s infinite");
+    } else {
+      setAnimation("");
+    }
+    setStatus(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <style>
+        {`@keyframes flash {
+          0%,100% {
+            opacity: 1;
+          }
+          50% {
+            opacity: 0;
+          }
+        }`}
+      </style>
+
+      <p>
+        <li style={{ display: "inline-block", color: "red", animation: animation }}>●</li>
+        <li style={{ display: "inline-block", color: "orange", animation: animation }}>●</li>
+        <li style={{ display: "inline-block", color: "yellow", animation: animation }}>●</li>
+        <li style={{ display: "inline-block", color: "green", animation: animation }}>●</li>
+        <li style={{ display: "inline-block", color: "lightblue", animation: animation }}>●</li>
+        <li style={{ display: "inline-block", color: "blue", animation: animation }}>●</li>
+        <li style={{ display: "inline-block", color: "purple", animation: animation }}>●</li>
+      </p>
+      <p>
+        <button onClick={() => onClickHandler("on")}>on</button>
+        <button onClick={() => onClickHandler("off")}>off</button>
+      </p>
+      <p>明滅の間隔</p>
+    </>
   );
-}
+};
 
 export default App;
