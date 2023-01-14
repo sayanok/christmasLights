fb0d9e18a801d3c772042f0bc30cdf975e4dd150
bigen1925 <bigen1925@gmail.com>
2023-01-14 12:12:09 +0900

    modify
    チカチカ

--- christmas-lights/src/App.tsx
+++ christmas-lights/src/App.tsx
@@ -3,15 +3,24 @@ import React, { useState } from "react";
 const App: React.FC = () => {
   const [status, setStatus] = useState("off");
   const [animation, setAnimation] = useState("");
+  const [animationBackBeat, setAnimationBackBeat] = useState("");
   const [interval, setInterval] = useState<number>();
 
-  function onClickHandler(value: string): void {
-    if (value === "on") {
+  function onClickHandler(switchStatus: string): void {
+    if (switchStatus === "on") {
       setAnimation("flash " + interval + "s infinite");
+      setAnimationBackBeat("flash-backbeat " + interval + "s infinite");
     } else {
       setAnimation("");
+      setAnimationBackBeat("");
     }
-    setStatus(value);
+    setStatus(switchStatus);
+  }
+
+  function onChangeHandler(numberOfSeconds: number): void {
+    setInterval(numberOfSeconds);
+    setAnimation("flash " + interval + "s infinite");
+    setAnimationBackBeat("flash-backbeat " + interval + "s infinite");
   }
 
   return (
@@ -25,15 +34,23 @@ const App: React.FC = () => {
             opacity: 0;
           }
         }`}
+        {`@keyframes flash-backbeat {
+          0%,100% {
+            opacity: 0;
+          }
+          50% {
+            opacity: 1;
+          }
+        }`}
       </style>
 
       <p>
         <li style={{ display: "inline-block", color: "red", animation: animation }}>●</li>
-        <li style={{ display: "inline-block", color: "orange", animation: animation }}>●</li>
+        <li style={{ display: "inline-block", color: "orange", animation: animationBackBeat }}>●</li>
         <li style={{ display: "inline-block", color: "yellow", animation: animation }}>●</li>
-        <li style={{ display: "inline-block", color: "green", animation: animation }}>●</li>
+        <li style={{ display: "inline-block", color: "green", animation: animationBackBeat }}>●</li>
         <li style={{ display: "inline-block", color: "lightblue", animation: animation }}>●</li>
-        <li style={{ display: "inline-block", color: "blue", animation: animation }}>●</li>
+        <li style={{ display: "inline-block", color: "blue", animation: animationBackBeat }}>●</li>
         <li style={{ display: "inline-block", color: "purple", animation: animation }}>●</li>
       </p>
       <p>
@@ -42,11 +59,7 @@ const App: React.FC = () => {
       </p>
       <form>
         <p>
-          <input
-            type={"number"}
-            value={interval}
-            onChange={(e) => setAnimation("flash " + Number(e.target.value) + "s infinite")}
-          ></input>
+          <input type={"number"} value={interval} onChange={(e) => onChangeHandler(Number(e.target.value))}></input>
           sec
         </p>
       </form>
